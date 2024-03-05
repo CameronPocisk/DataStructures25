8003813a8f1588bbecf41a791dc4283be8d6cfee
Jake Skaff <skaffjm@mail.uc.edu>
2024-03-05 16:00:05 -0500

    get compiling soon

--- Homework3/Homework3.cpp
+++ Homework3/Homework3.cpp
@@ -1,6 +1,8 @@
 // #include "LinkedList.h"
 #include "DeckQueue.h"
 #include "PileStack.h"
+// #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+// #include "doctest.h"
 
 #include<iostream>
 using namespace std;
@@ -34,11 +36,13 @@ void distributeCards(DeckQueue p, DeckQueue cpu){
         p.Enqueue(deck[i]);
         cpu.Enqueue(deck[i+1]);
     }
+    cout << "Shuffled" << endl;
     
 }
 
 
 int main(){
+    cout << "Running file" << endl;
     srand(time(0));
 
     // initilizations
@@ -52,56 +56,114 @@ int main(){
     int userChoice;
     int pCurCard;
     int cpuCurCard;
-    while(pDeck.length() != 0 && cDeck.length() != 0){
-        
-        cout << "The first card you pulled is a: " << pDeck.Dequeue() << endl;
+    int pSideCard;
+    int cpuSideCard;
+
+    while((pDeck.length() != 0 || pSide.GetNumCards() != 0) && (cDeck.length() != 0 || pSide.GetNumCards() != 0)){
+        cout << "In While" << endl;
+        if(pDeck.length() != 0){
+            pCurCard = pDeck.Dequeue();
+        }
+        else{
+            pCurCard = pSide.RemoveCard();
+        }
+        if(cDeck.length() != 0){
+            cpuCurCard = cDeck.Dequeue();
+        }
+        else{
+            cpuCurCard = cSide.RemoveCard();
+        }
+
+
+        cout << "The first card you pulled is a: " << pCurCard << endl;
         cout << "Press 1 to play this card." << endl << "Press 2 to pull from the side pile." << endl << "Press 3 to push to the side pile." << endl;
         cin >> userChoice;
         //Add exception
-        while(userChoice != 1 && userChoice != 2 && userChoice != 3){
-            cout << "Please type 1, 2, or 3: ";
-            cin >> userChoice;
-        }
+        // while(userChoice != 1 && userChoice != 2 && userChoice != 3){
+        //     cout << "Please type 1, 2, or 3: ";
+        //     cin >> userChoice;
+        // }
 
-        switch(userChoice){
-            case 1:
-                cDeck.Dequeue()
+        // switch(userChoice){
+        //     case 1:
+        //         if(pCurCard > cpuCurCard){
+        //             cout << "Your " << pCurCard << " is higher then the cpu card of " << cpuCurCard << endl;
+        //             pDeck.Enqueue(pCurCard);
+        //             pDeck.Enqueue(cpuCurCard);
+        //         } else {
+        //             cout << "The cpu pulled " << cpuCurCard << " which is higher or equal to " << pCurCard << endl;
+        //             cDeck.Enqueue(pCurCard);
+        //             cDeck.Enqueue(cpuCurCard);
+        //         }
                 
-        }
+        //         break;
+        //     case 2:
+        //         pSideCard = pSide.RemoveCard();
+        //         if(pCurCard + pSideCard > cpuCurCard) {
+        //             cout << "Your " << pCurCard << " + " << pSideCard << " is higher than the cpu's " << cpuCurCard << endl;
+        //             pDeck.Enqueue(pCurCard);
+        //             pDeck.Enqueue(cpuCurCard);
+        //             pDeck.Enqueue(pSideCard);
+        //         } else {
+        //             cout << "The cpu pulled " << cpuCurCard << " which is higher or equal to " << pCurCard << " + " << pSideCard;
+        //             cDeck.Enqueue(pCurCard);
+        //             cDeck.Enqueue(cpuCurCard);
+        //             cDeck.Enqueue(pSideCard);
+        //         }
+        //         break;
+        //     case 3:
+        //         pSide.AddCard(pCurCard);
+        //         pCurCard = pDeck.Dequeue();
+        //         if(pCurCard > cpuCurCard){
+        //             cout << "Your " << pCurCard << " is higher then the cpu card of " << cpuCurCard << endl;
+        //             pDeck.Enqueue(pCurCard);
+        //             pDeck.Enqueue(cpuCurCard);
+        //         } else {
+        //             cout << "The cpu pulled " << cpuCurCard << " which is higher or equal to " << pCurCard << endl;
+        //             cDeck.Enqueue(pCurCard);
+        //             cDeck.Enqueue(cpuCurCard);
+        //         }
+        //         break;
+        // }
 
-
-
-
-        cout << "Press 1 to check how many cards are in your deck." << endl << "Press 2 to check how many cards are in the computer's deck." << endl << "Press 3 to check both" << endl << "Press 4 to continue." << endl;
-        cin >> userChoice;
-        while(userChoice != 1 && userChoice != 2 && userChoice != 3 && userChoice != 4){
-            cout << "Please type 1, 2, 3, or 4: ";
-            cin >> userChoice;
-        }
-        switch (userChoice)
-        {
-        case 1:
-            cout << "There are " << pDeck.length() << " cards in your deck." << endl;
-            break;
-        case 2:
-            cout << "There are " << cDeck.length() << " cards in the computer's deck." << endl;
-            break;
-        case 3:
-            cout << "There are " << pDeck.length() << " cards in your deck, and " << cDeck.length() << " cards in the computer's deck." << endl;
+        // cout << "Press 1 to check how many cards are in your deck." << endl << "Press 2 to check how many cards are in the computer's deck." << endl << "Press 3 to check both" << endl << "Press 4 to continue." << endl;
+        // cin >> userChoice;
+        // while(userChoice != 1 && userChoice != 2 && userChoice != 3 && userChoice != 4){
+        //     cout << "Please type 1, 2, 3, or 4: ";
+        //     cin >> userChoice;
+        // }
+        // switch (userChoice)
+        // {
+        // case 1:
+        //     cout << "There are " << pDeck.length() << " cards in your deck." << endl;
+        //     break;
+        // case 2:
+        //     cout << "There are " << cDeck.length() << " cards in the computer's deck." << endl;
+        //     break;
+        // case 3:
+        //     cout << "There are " << pDeck.length() << " cards in your deck, and " << cDeck.length() << " cards in the computer's deck." << endl;
         
-        default:
-            break;
-        }
+        // default:
+        //     break;
+        // }
     }
-
-    if(pDeck.length() )
     
-    
+    // // Winner message
+    // if(pDeck.length() != 0){
+    //     cout << "************" << endl;
+    //     cout << "Player wins!";
+    //     cout << "************" << endl;
+    // }
+    // else{
+    //     cout << "********" << endl;
+    //     cout << "Cpu won (you suck :/)";
+    //     cout << "********" << endl;
+    // }
 
     return 0;
 }
 
-// g++ -c Homework3.cpp LinkedList.h DeckQueue.cpp PileStack.cpp
+// g++ -c Homework3.cpp LinkedList.cpp DeckQueue.cpp PileStack.cpp
 // g++ -o main.exe Homework3.o LinkedList.o DeckQueue.o PileStack.o
 // ./main.exe
 
