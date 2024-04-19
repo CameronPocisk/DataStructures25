f5d5ca29f1e71eafe910c6a760e6f0db2d67f69b
Iguanx <Iquinadom@gmail.com>
2024-04-19 10:49:34 -0400

    only need finishing touched

--- HuntOfWumpusDom/HuntOfWumpus.cpp
+++ HuntOfWumpusDom/HuntOfWumpus.cpp
@@ -13,22 +13,21 @@ int main() {
     bool gameContinue = true;
     while(gameContinue){
         try{
-
-            cout << "You are in room " << main.getPlayerRoom() << endl;
-            main.displayOutEdges(main.getPlayerRoom() - 1);
             char userChoice = 'a';
-            while(toupper(userChoice) != 'M' && toupper(userChoice) != 'S'){
-                cout << "Enter 'M' to move, or 'S' to shoot: ";
+            while(toupper(userChoice) != 'M' && toupper(userChoice) != 'S' && toupper(userChoice) != 'O'){
+                cout << "Commands: Move ( M ), Shoot ( S ), or Observe ( O )." << endl;
+                cout << "You are in room " << main.getPlayerRoom() << endl;
+                cout << "What would you like to do: ";
                 cin >> userChoice;
             }
             if(toupper(userChoice) == 'M'){
                 main.Move();
-            } else{
+            } else if(toupper(userChoice) == 'S'){
                 main.Shoot();
                 main.PrintMap();
+            } else {
+                main.displayOutEdges(main.getPlayerRoom() - 1);
             }
-
-            main.printArrows();
 
         }
         catch(DeadError &e){
